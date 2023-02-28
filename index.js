f961b4c48e6d575cbf483a8e82aef8581d12e8aa
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2023-02-28 23:40:30 +0530

    CS S6

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-
-btn1.addEventListener("click", function() {
-let selectElement = document.getElementById("opt1");
-let selectedValue = selectElement.value;
-if (selectedValue === "SGPA") {
-    console.log("SGPA");}
-  else if (selectedValue === "CGPA") {
-      console.log("CGPA");}
-  });
-
-
 var data = {
     branches: [
         {
@@ -25,9 +14,198 @@ var data = {
                             name: "Physics/Chemistry",
                             credits: 4
                         },
+                        {
+                            name: "Graphics/Mechanics",
+                            credits: 3
+                        },
+                        {
+                            name: "BCME/BEEE",
+                            credits: 4
+                        },
+                        {
+                            name: "Phy/Che Lab",
+                            credits: 1
+                        },
+                        {
+                            name: "C&M Workshop/ E&E Workshop",
+                            credits: 1
+                        }
+
                     ]
-                }
-                
+                },
+                {
+                    name: "Semester 2",
+                    subjects: [
+                        {
+                            name: "Maths",
+                            credits: 4
+                        },
+                        {
+                            name: "Physics/Chemistry",
+                            credits: 4
+                        },
+                        {
+                            name: "Graphics/Mechanics",
+                            credits: 3
+                        },
+                        {
+                            name: "BCME/BEEE",
+                            credits: 4
+                        },
+                        {
+                            name: "Programming in C",
+                            credits: 3
+                        },
+                        {
+                            name: "Phy/Che Lab",
+                            credits: 1
+                        },
+                        {
+                            name: "C&M Workshop/ E&E Workshop",
+                            credits: 1
+                        }
+
+                    ]
+                },
+                {
+                    name: "Semester 3",
+                    subjects: [
+                        {
+                            name: "DISCRETE MATHEMATICAL STRUCTURES",
+                            credits: 4
+                        },
+                        {
+                            name: "DATA STRUCTURES",
+                            credits: 4
+                        },
+                        {
+                            name: "OBJECT ORIENTED PROGRAMMING USING JAVA",
+                            credits: 4
+                        },
+                        {
+                            name: "LOGIC SYSTEMS DESIGN",
+                            credits: 4
+                        },
+                        {
+                            name: "DESIGN AND ENGINEERING",
+                            credits: 2
+                        },
+                        {
+                            name: "DATA STRUCTURES LAB",
+                            credits: 2
+                        },
+                        {
+                            name: "OBJECT ORIENTED PROGRAMMING USING JAVA LAB",
+                            credits: 2
+                        }
+                    ]  
+                },
+                {
+                    name: "Semester 4",
+                    subjects: [
+                        {
+                            name: "GRAPH THEORY",
+                            credits: 4
+                        },
+                        {
+                            name: "Computer Organization and Architecture",
+                            credits: 4
+                        },
+                        {
+                            name: "Database Management Systems",
+                            credits: 4
+                        },
+                        {
+                            name: "OPERATING SYSTEMS",
+                            credits: 4
+                        },
+                        {
+                            name: "PROFESSIONAL ETHICS",
+                            credits: 2
+                        },
+                        {
+                            name: "DIGITAL LAB",
+                            credits: 2
+                        },
+                        {
+                            name: "OPERATING SYSTEMS LAB",
+                            credits: 2
+                        }
+                    ]   
+                },
+                {
+                    name: "Semester 5",
+                    subjects: [
+                        {
+                            name: "FORMAL LANGUAGES AND AUTOMATA THEORY",
+                            credits: 4
+                        },
+                        {
+                            name: "COMPUTER NETWORKS",
+                            credits: 4
+                        },
+                        {
+                            name: "SYSTEM SOFTWARE",
+                            credits: 4
+                        },
+                        {
+                            name: "MICROPROCESSORS AND MICROCONTROLLERS",
+                            credits: 4
+                        },
+                        {
+                            name: "MANAGEMEENT OF SOFTWARE SYSTEMS",
+                            credits: 3
+                        },
+                        {
+                            name: "SYSTEM SOFTWARE AND MICROPROCESSORS LAB",
+                            credits: 2
+                        },
+                        {
+                            name: "DATABASE MANAGEMENT SYSTEMS LAB",
+                            credits: 2
+                        }
+                    ]   
+                },
+                {
+                    name: "Semester 6",
+                    subjects: [
+                        {
+                            name: "COMPILER DESIGN",
+                            credits: 4
+                        },
+                        {
+                            name: "COMPUTER GRAPHICS AND IMAGE PROCESSING",
+                            credits: 4
+                        },
+                        {
+                            name: "ALGORITHM ANALYSIS AND DESIGN",
+                            credits: 4
+                        },
+                        {
+                            name: "ELECTIVE 1",
+                            credits: 3
+                        },
+                        {
+                            name: "INDUSTRIAL ECONOMICS AND FOREIGN TRADE",
+                            credits: 3
+                        },
+                        {
+                            name: "COMPREHENSIVE COURSE WORK",
+                            credits: 1
+                        },
+                        {
+                            name: "NETWORKING LAB",
+                            credits: 2
+                        },
+                        {
+                            name: "MINI PROJECT",
+                            credits: 2
+                        }
+                    ]   
+                },
+
+
+
             ]
         }
     ]
