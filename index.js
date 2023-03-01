f6be225c2367eb86e582ed2d1d7b59eb2cc23d0b
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2023-03-01 22:19:35 +0530

    Anjitha

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 var data = {
     branches: [
         {
@@ -207,6 +209,236 @@ var data = {
 
 
             ]
+        },
+        {
+            name: "ECE",
+            semesters: [
+                {
+                    name: "Semester 1",
+                    subjects: [
+                        {
+                            name: "Maths",
+                            credits: 4
+                        },
+                        {
+                            name: "Physics/Chemistry",
+                            credits: 4
+                        },
+                        {
+                            name: "Graphics/Mechanics",
+                            credits: 3
+                        },
+                        {
+                            name: "BCME/BEEE",
+                            credits: 4
+                        },
+                        {
+                            name: "Phy/Che Lab",
+                            credits: 1
+                        },
+                        {
+                            name: "C&M Workshop/ E&E Workshop",
+                            credits: 1
+                        }
+
+                    ]
+                },
+                {
+                    name: "Semester 2",
+                    subjects: [
+                        {
+                            name: "Maths",
+                            credits: 4
+                        },
+                        {
+                            name: "Physics/Chemistry",
+                            credits: 4
+                        },
+                        {
+                            name: "Graphics/Mechanics",
+                            credits: 3
+                        },
+                        {
+                            name: "BCME/BEEE",
+                            credits: 4
+                        },
+                        {
+                            name: "Programming in C",
+                            credits: 3
+                        },
+                        {
+                            name: "Phy/Che Lab",
+                            credits: 1
+                        },
+                        {
+                            name: "C&M Workshop/ E&E Workshop",
+                            credits: 1
+                        }
+
+                    ]
+                },
+                {
+                    name: "Semester 3",
+                    subjects: [
+                        {
+                            name: "DISCRETE MATHEMATICAL STRUCTURES",
+                            credits: 4
+                        },
+                        {
+                            name: "DATA STRUCTURES",
+                            credits: 4
+                        },
+                        {
+                            name: "OBJECT ORIENTED PROGRAMMING USING JAVA",
+                            credits: 4
+                        },
+                        {
+                            name: "LOGIC SYSTEMS DESIGN",
+                            credits: 4
+                        },
+                        {
+                            name: "DESIGN AND ENGINEERING",
+                            credits: 2
+                        },
+                        {
+                            name: "DATA STRUCTURES LAB",
+                            credits: 2
+                        },
+                        {
+                            name: "OBJECT ORIENTED PROGRAMMING USING JAVA LAB",
+                            credits: 2
+                        }
+                    ]  
+                },
+                {
+                    name: "Semester 4",
+                    subjects: [
+                        {
+                            name: "GRAPH THEORY",
+                            credits: 4
+                        },
+                        {
+                            name: "Computer Organization and Architecture",
+                            credits: 4
+                        },
+                        {
+                            name: "Database Management Systems",
+                            credits: 4
+                        },
+                        {
+                            name: "OPERATING SYSTEMS",
+                            credits: 4
+                        },
+                        {
+                            name: "PROFESSIONAL ETHICS",
+                            credits: 2
+                        },
+                        {
+                            name: "DIGITAL LAB",
+                            credits: 2
+                        },
+                        {
+                            name: "OPERATING SYSTEMS LAB",
+                            credits: 2
+                        }
+                    ]   
+                },
+                {
+                    name: "Semester 5",
+                    subjects: [
+                        {
+                            name: "FORMAL LANGUAGES AND AUTOMATA THEORY",
+                            credits: 4
+                        },
+                        {
+                            name: "COMPUTER NETWORKS",
+                            credits: 4
+                        },
+                        {
+                            name: "SYSTEM SOFTWARE",
+                            credits: 4
+                        },
+                        {
+                            name: "MICROPROCESSORS AND MICROCONTROLLERS",
+                            credits: 4
+                        },
+                        {
+                            name: "MANAGEMEENT OF SOFTWARE SYSTEMS",
+                            credits: 3
+                        },
+                        {
+                            name: "SYSTEM SOFTWARE AND MICROPROCESSORS LAB",
+                            credits: 2
+                        },
+                        {
+                            name: "DATABASE MANAGEMENT SYSTEMS LAB",
+                            credits: 2
+                        }
+                    ]   
+                },
+                {
+                    name: "Semester 6",
+                    subjects: [
+                        {
+                            name: "COMPILER DESIGN",
+                            credits: 4
+                        },
+                        {
+                            name: "COMPUTER GRAPHICS AND IMAGE PROCESSING",
+                            credits: 4
+                        },
+                        {
+                            name: "ALGORITHM ANALYSIS AND DESIGN",
+                            credits: 4
+                        },
+                        {
+                            name: "ELECTIVE 1",
+                            credits: 3
+                        },
+                        {
+                            name: "INDUSTRIAL ECONOMICS AND FOREIGN TRADE",
+                            credits: 3
+                        },
+                        {
+                            name: "COMPREHENSIVE COURSE WORK",
+                            credits: 1
+                        },
+                        {
+                            name: "NETWORKING LAB",
+                            credits: 2
+                        },
+                        {
+                            name: "MINI PROJECT",
+                            credits: 2
+                        }
+                    ]   
+                },
+
+
+
+            ]
         }
+            
+        
+
     ]
 }
+var container = document.getElementById("secondline");
+var html = "";
+    html += "<select name = 'branch' id = 'branch'>"
+    data.branches.forEach(function(branch){
+    html += "<option>"+ branch.name +"</option>";
+    console.log(branch.name);})
+    html += "</select>";
+    html += "<select name = 'semester' id = 'semester'>"
+        data.branches.forEach(function(branch){
+            branch.semesters.forEach(function(semester){
+                html += "<option>"+ semester.name+"</option>"; 
+            });
+        });
+    html += "</select>";
+
+
+container.innerHTML = html;
+
+
